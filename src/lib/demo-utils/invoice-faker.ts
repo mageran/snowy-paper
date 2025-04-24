38a1884a557d2701fd5d6df78a586d101b0c5a6b
Matthias Anlauff <matthiasanlauff@gmail.com>
2025-04-24 13:44:16 -0700

    fixed fake data generation for invoices

--- src/lib/demo-utils/invoice-faker.ts
+++ src/lib/demo-utils/invoice-faker.ts
@@ -1,18 +1,32 @@
 import { InvoiceObject, InvoiceStatusValues } from "../../components/Invoice/invoice";
 import { faker } from "@faker-js/faker";
-import { dateInNDays } from "../utils";
 
 export const createFakeInvoice = (): InvoiceObject => {
     const id = `INV-${faker.string.numeric(5)}`;
     const currency = 'USD';
     const value = faker.finance.amount({ min: 20, max: 5000 });
-    const status = faker.helpers.arrayElement(InvoiceStatusValues);
+    let status = faker.helpers.arrayElement(InvoiceStatusValues);
     const customerName = faker.company.name();
-    const notes = faker.lorem.words({ min: 3, max: 8});
-    const invoiceDate = faker.date.recent({ days: 90 });
+    const notes = faker.lorem.words({ min: 3, max: 8 });
+    let invoiceDate = faker.date.recent({ days: 180 });
+    if (status === 'draft' || status === 'pendingApproval') {
+        invoiceDate = faker.date.soon({ days: 5 })
+    }
     const dueInDays = faker.helpers.arrayElement([45, 60, 90]);
-    const dueDate = new Date(invoiceDate);
+    let dueDate = new Date(invoiceDate);
     dueDate.setDate(dueDate.getDate() + dueInDays);
+    if (status !== 'draft' && status !== 'pendingApproval') {
+        const today = new Date();
+        if (today > dueDate && status !== 'paid') {
+            status = 'pastDue';
+        }
+        if (today < dueDate && status !== 'paid') {
+            status = 'awaitingPayment';
+        }
+        if (today.toLocaleDateString() === dueDate.toLocaleDateString() && status !== 'paid') {
+            status = 'due';
+        }
+    }
     return InvoiceObject.createInvoiceFromRecord({
         id,
         currency,
